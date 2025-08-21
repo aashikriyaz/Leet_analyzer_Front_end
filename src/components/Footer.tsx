de5daf0d71d04831ff5e9a208a31f9eb01344e61
aashikriyaz <165645036+aashikriyaz@users.noreply.github.com>
2025-08-21 19:19:48 +0530

    ANY_MESSAGE

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="w-full mt-auto bg-black text-white text-center py-6 text-sm md:text-base shadow-inner">
-        created by Aashik Riyaz M, LeetLens.
+        created by Aashik Riyaz M, LeetAnalyzer.
       </footer>
     );
   };
